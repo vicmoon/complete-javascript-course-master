bf6533613c322e3fcb790923a20c7ecff61da483
Victoria _Munteanu <munteanuvictoria1@gmail.com>
2023-11-07 20:34:16 +0100

    Initial commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,53 @@
+     // let js = 'amazing';
+    //  if(js === 'amazing') alert('JavaSript is fun!');      
+
+
+// 2. Values and Variables
+/*
+let country = "Czechia";
+console.log(country);
+let continent = "Europe";
+console.log(continent);
+let population = 10.7; 
+console.log(population);      
+*/
+
+// 3. Data Types
+/*
+let age = 30;
+age  =31;  // reassigning the variable, mutating the variable 
+const birthYear = 1991; // const is immutable, cannot be changed 
+
+var job = 'programmer'; // var is old way of declaring variables, not used anymore 
+job = 'teacher'; // reassigning the variable, mutating the variable
+*/
+
+// 4. Basic Operators
+/*
+const now = 2037;
+const ageVictoria = now - 1991; 
+const ageSarah = now - 2018; 
+console.log(ageVictoria, ageSarah); 
+console.log(ageVictoria * 2, ageVictoria / 10, 2 ** 3); // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2  
+
+let x = 10 + 5; // 15
+x += 10 // x = x + 10 = 25 
+console.log(x);
+x *= 4; // x = x * 4 = 100 
+console.log(x); 
+x++; // x = x + 1
+console.log(x); 
+x--; // x = x - 1 
+console.log(ageVictoria > ageSarah); // >, <, >=, <=  
+console.log(ageSarah >= 18);
+*/ 
+
+// 5. Operator Precedence
+
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10, y = 10 
+console.log(x, y); // 10, 10 
+const averageAge = (ageVictoria + ageSarah) / 2; 
+
+
